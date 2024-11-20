734e3317ab4c4704d6887306801385d25b83db68
dylanson25 <dylanson25@users.noreply.github.com>
2024-11-19 19:17:03 -0800

    [#891] - Implement media resource component

--- src/app/components/media-resource-tags/media-resource-tags.component.ts
+++ src/app/components/media-resource-tags/media-resource-tags.component.ts
@@ -1,39 +1,17 @@
 import { Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Media } from '@models/media.model';
+import { TooltipDirective } from '../../directives/tooltip.directive';
+import { MediaResourceTagComponent } from '../media-resource-tag/media-resource-tag.component';
 
 @Component({
 	selector: 'cuentoneta-media-resource-tags',
 	standalone: true,
-	imports: [CommonModule],
+	hostDirectives: [TooltipDirective],
+	imports: [CommonModule, MediaResourceTagComponent],
 	template: ` @for (mediaResource of resources(); track $index) {
-		@switch (mediaResource.type) {
-			@case ('audioRecording') {
-				<div [title]="'Contiene narraciones en audio'" [class]="size()" class="flex items-center justify-center">
-					<img [alt]="'Contiene narraciones en audio'" src="assets/svg/waveform.svg" />
-				</div>
-			}
-			@case ('spaceRecording') {
-				<div [title]="'Contiene grabaciones de Spaces de X'" [class]="size()" class="flex items-center justify-center">
-					<img [alt]="'Contiene grabaciones de Spaces de X'" src="assets/svg/twitter.svg" />
-				</div>
-			}
-			@case ('youTubeVideo') {
-				<div [title]="'Contiene videos de YouTube'" [class]="size()" class="flex items-center justify-center">
-					<img [alt]="'Contiene videos de YouTube'" src="assets/svg/video.svg" />
-				</div>
-			}
-		}
+		<cuentoneta-media-resource-tag [mediaresourcetype]="mediaResource.type" [size]="size()" />
 	}`,
-	styles: `
-		.md {
-			@apply h-6 w-6;
-		}
-
-		.lg {
-			@apply h-8 w-8;
-		}
-	`,
 })
 export class MediaResourceTagsComponent {
 	resources = input<Media[]>([]);
